81e8d40ffdb06dfa6798fb9b8441be9fc6a6e15d
EldarAkh04 <114343028+EldarAkh04@users.noreply.github.com>
2026-03-16 14:12:42 +0100

    for loop through the generated images

--- CLIP_Modell/CLIP.py
+++ CLIP_Modell/CLIP.py
@@ -6,23 +6,27 @@ import os
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-image_path = "~/Desktop/8. Semester/BachelorThesis/replicate/images/squirrel_cow/hybrid_1.png"
-#image = Image.open(requests.get(image_path, stream=True).raw)
-image = os.path.expanduser(image_path)
+folder_path = "~/Desktop/8. Semester/BachelorThesis/replicate/images/squirrel_cow"
+#image = Image.open(requests.get(folder_path, stream=True).raw)
+image_path = os.path.expanduser(folder_path)
+image_list = os.listdir(image_path)
 
-labels = ["a cat","a dog"]
+for filename in image_list:
+    if(filename).startswith("."): continue
+    full_image_path = os.path.join(image_path, filename)
 
-inputs = processor(text = labels, images=image, return_tensors="pt", padding=True)
+    current_image = Image.open(full_image_path)
+    labels = ["a cat","a dog"]
 
-outputs = model(**inputs)
-logits_per_image = outputs.logits_per_image
-probs = logits_per_image.softmax(dim=1)
+    inputs = processor(text = labels, images=current_image, return_tensors="pt", padding=True)
+    outputs = model(**inputs)
+    probs = outputs.logits_per_image.softmax(dim=1)
 
-print("\n" + "="*30)
-print(f"ANALYSE FÜR: {image_path}")
-print("="*30)
+    print("\n" + "="*30)
+    print(f"ANALYSE FÜR: {folder_path}")
+    print("="*30)
 
-for i, label in enumerate(labels):
-    percentage = probs[0][i].item() * 100
-    print(f"{label:30} : {percentage:>6.2f}%")
-print("="*30)
+    for i, label in enumerate(labels):
+        percentage = probs[0][i].item() * 100
+        print(f"{label:30} : {percentage:>6.2f}%")
+    print("="*30)
